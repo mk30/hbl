8a7064ee0c7e87cff4aa45539e1dff70838d7012
Marina Kukso <marina.kukso@gmail.com>
2015-12-08 23:34:49 -0800

    tcp-bind, package.json

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var ecstatic = require('ecstatic');
+var alloc = require('tcp-bind');
+var fd = alloc(80);
+
+process.setgid(process.argv[3]);
+process.setuid(process.argv[2]);
  
 http.createServer(
   ecstatic({ root: __dirname + '/public' })
-).listen(8080);
- 
-console.log('Listening on :8080');
+).listen({ fd: fd });
